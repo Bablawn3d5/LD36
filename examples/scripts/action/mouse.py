edbd19632a0b3b595844b979cb149dd4dee4160b
Stephen Ma <tehphen@gmail.com>
2016-08-27 15:08:17 -0700

    Fix speed on slower computers.

--- examples/scripts/action/mouse.py
+++ examples/scripts/action/mouse.py
@@ -12,7 +12,7 @@ class MouseFollower(Follower):
         self.body = self.Component(Body)
         self.body.position.x = 240
         self.body.position.y = 240
-        
+
         self.physics = self.Component(Physics)
         self.physics.bodyType = b2BodyType.DYNAMIC
         self.physics.size.x = 10
@@ -21,7 +21,7 @@ class MouseFollower(Follower):
         self.physics.mask.bits = CollisionCategory.CATEGORY_16
         self.physics.isSensor = True
         self.physics.isBullet = True
-        
+
         self.stats = self.Component(Stats)
         self.is_clicking = False
         self.click_cooldown = 0.1
@@ -29,20 +29,24 @@ class MouseFollower(Follower):
 
     def update(self, dt):
         self.is_clicking = False
-    
+
         mousePos = Vector2(self.inresponder.mousePos)
-        
+
         cur_pos = Vector2(self.body.position)
         dest_pos = mousePos
 
         distance_to_travel = math.sqrt(math.pow(dest_pos.get_x() - cur_pos.get_x(), 2) + math.pow(dest_pos.get_y() - cur_pos.get_y(), 2))
-        new_speed = (distance_to_travel / dt) / 50
-        self.stats.speed = new_speed
-        
+        # If close enough just set speed to 0, else smooth speed to dest
+        if(distance_to_travel > 0.05 and dt > 0):
+            new_speed = (distance_to_travel / dt) / 15
+            self.stats.speed = new_speed
+        else:
+            self.stats.speed = 0
+
         direction_vec = dest_pos - cur_pos
         direction_vec.normalize()
         direction_vec.copy_to(self.body.direction)
-        
+
         input_events = self.inresponder.responds
         if "+Spawn" in input_events and self.physics.collisionCount > 0:
             self.is_clicking = False
