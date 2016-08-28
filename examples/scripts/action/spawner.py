a0f93cda6ff81887bd1f9decb727800bb39556be
Stephen Ma <tehphen@gmail.com>
2016-08-27 19:24:26 -0700

    Add some more images.

--- examples/scripts/action/spawner.py
+++ examples/scripts/action/spawner.py
@@ -2,6 +2,7 @@ import entityx
 from _entityx_components import CollisionCategory, Body, Renderable, InputResponder, Physics, Stats, b2BodyType
 from follower import Follower
 from gamemath import vector2
+from random import randint
 
 Vector2 = vector2.Vector2
 
@@ -29,46 +30,46 @@ class MagicSpawner():
     def spawnStix(self, spawn_pos, dest_pos, size, speed):
         e = self.spawnThing(spawn_pos, dest_pos, size, speed)
         rend = e.Component(Renderable)
-        rend.texture = "./images/stick_1.png"
+        rend.texture = "./images/stick_%d.png" % (randint(1,2))
 
     @classmethod
     def spawnTree(self, spawn_pos, dest_pos, size, speed):
         e = self.spawnThing(spawn_pos, dest_pos, size, speed)
         rend = e.Component(Renderable)
-        rend.texture = "./images/stick_1.png"
+        rend.texture = "./images/tree_%d.png" % (randint(1,1))
 
     @classmethod
     def spawnPeople(self, spawn_pos, dest_pos, size, speed):
         e = self.spawnThing(spawn_pos, dest_pos, size, speed)
         rend = e.Component(Renderable)
-        rend.texture = "./images/stick_1.png"
+        rend.texture = "./images/person_%d.png" % (randint(1,1))
 
     @classmethod
     def spawnBuilding(self, spawn_pos, dest_pos, size, speed):
         e = self.spawnThing(spawn_pos, dest_pos, size, speed)
         rend = e.Component(Renderable)
-        rend.texture = "./images/stick_1.png"
+        rend.texture = "./images/house_%d.png" % (randint(1,1))
 
     @classmethod
     def spawnCity(self, spawn_pos, dest_pos, size, speed):
         e = self.spawnThing(spawn_pos, dest_pos, size, speed)
         rend = e.Component(Renderable)
-        rend.texture = "./images/stick_1.png"
+        rend.texture = "./images/city_%d.png" % (randint(1,1))
 
     @classmethod
     def spawnPlanet(self, spawn_pos, dest_pos, size, speed):
         e = self.spawnThing(spawn_pos, dest_pos, size, speed)
         rend = e.Component(Renderable)
-        rend.texture = "./images/stick_1.png"
+        rend.texture = "./images/planet_%d.png" % (randint(1,6))
 
     @classmethod
     def spawnGalaxy(self, spawn_pos, dest_pos, size, speed):
         e = self.spawnThing(spawn_pos, dest_pos, size, speed)
         rend = e.Component(Renderable)
-        rend.texture = "./images/stick_1.png"
+        rend.texture = "./images/galaxy_%d.png" % (randint(1,3))
 
     @classmethod
     def spawnContinent(self, spawn_pos, dest_pos, size, speed):
         e = self.spawnThing(spawn_pos, dest_pos, size, speed)
         rend = e.Component(Renderable)
-        rend.texture = "./images/stick_1.png"
+        rend.texture = "./images/content_%d.png" % (randint(1,10))
