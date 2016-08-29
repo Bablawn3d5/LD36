35e2612b8454a176a641ffae0fbe5cf3549a5fd3
Stephen Ma <tehphen@gmail.com>
2016-08-28 20:30:21 -0700

    Time object death to ending

--- examples/scripts/action/spawner.py
+++ examples/scripts/action/spawner.py
@@ -17,9 +17,9 @@ class MagicSpawner():
         e.totalTime = uniform(1,3.75)
         bod = e.Component(Body)
         spawn_pos.copy_to(bod.position)
-        # Make it die after a minute
+        # Make it die after a 40 seconds for the ending.
         e.death = e.Component(Destroyed)
-        e.death.timer = 65
+        e.death.timer = 40
 
         stats = e.Component(Stats)
         stats.speed = speed
